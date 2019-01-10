3bb087e4187d82773f223ae23a2028296f9c3a28
Diana Gallego <geedia@gmail.com>
2019-01-10 18:29:13 -0500

    Creacion del proyecto

---
+++ src/main/java/com/dg/banco/bancodg/BancoDgApplication.java
@@ -0,0 +1,14 @@
+package com.dg.banco.bancodg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoDgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoDgApplication.class, args);
+	}
+
+}
+
